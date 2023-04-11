9d9a92cdce4c9e177b26a8522651ab71463c7370
Alexander <55991290+Kringiner@users.noreply.github.com>
2023-04-11 22:38:03 +0500

    attemp 1

--- solution.py
+++ solution.py
@@ -1,11 +1,10 @@
-import pandas as pd
-import numpy as np
+from scipy import stats
+
+chat_id = 843200348
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+def solution(x, y) -> bool:
+    alpha = 0.03
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    p_val = stats.ttest_ind(x, y).pvalue
+    return p_val < alpha
